a47ce139291f50aec91c6d66a31f33b9411b75c3
Petr Juna <petr.juna@gmail.com>
2024-04-29 22:47:39 +0200

    fix: clean channel

--- apps/playhouse/src/index.ts
+++ apps/playhouse/src/index.ts
@@ -10,8 +10,12 @@ const root = ReactDOM.createRoot(
 
 root.render(createElement(App));
 
+const channel = new MessageChannel();
+
 // Communicates with enclosing parent .po
 store.subscribe(() => {
   // FIXME: targetOrigin has to be adjusted for production
-  parent.postMessage(store.getState(), "http://localhost:3000");
+  channel.port1.postMessage(store.getState());
 });
+
+parent.postMessage({ id: "@channel/container" }, "*", [channel.port2]);

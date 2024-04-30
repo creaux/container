75567f6f76ae2e8f673e2525e5a6815a44c081ae
Petr Juna <petr.juna@gmail.com>
2024-04-30 21:48:43 +0200

    chore: comment removal

--- libs/worker/src/lib/src/App.tsx
+++ libs/worker/src/lib/src/App.tsx
@@ -1,4 +1,3 @@
-// eslint-disable-next-line @nx/enforce-module-boundaries
 import { store } from '@creaux/container-store';
 import { Provider } from 'react-redux';
 import { Component } from './component.assess';
